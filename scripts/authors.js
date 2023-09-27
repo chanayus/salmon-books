d9392b749cb0e5e4c4731a4c8ae832939041e79f
chanayus <chanayus1526@hotmail.com>
2023-09-27 17:00:34 +0700

    update authors filter

--- scripts/authors.js
+++ scripts/authors.js
@@ -1,65 +1,19 @@
 import { gravityHover } from "./modules/gravityHover.js";
-import { getSelectedData } from "./modules/menuFilter.js";
-
-const authors = [
-  "A GIRL LIKE YOU",
-  "Art Jeeno",
-  "Banana Blah Blah",
-  "Banana Yoshimoto",
-  "Beth O'Leary",
-  "C.S.",
-  "IANNNNN",
-  "juli baker and summer",
-  "LADYS",
-  "MALIMALI",
-  "Michelle Zauner",
-  "Miranda July",
-  "MISTER TOMPKIN",
-  "Ocean Vuong",
-  "PLARIEX",
-  "PPONG",
-  "SAHRED TOY",
-  "Sally Rooney",
-  "Team Salmon",
-  "TOMA",
-  "TUNA DUNN",
-  "uninspired by current events",
-  "wrongdesign",
-  "กรุณพร เชษฐพยัคฆ",
-  "กฤดิกร วงศ์สว่างพานิช",
-  "กิตติพล สรัคคานนท",
-  "ขวัญชาย ดำรงค์ขวัญ",
-  "คงเดช จาตุรันต์รัศมี",
-  "คันฉัตร รังษีกาญจน์ส่อง",
-  "คัมภีร์ สรวมศิริ",
-  "ฆนาธร ขาวสนิท",
-  "จักรพันธุ์ ขวัญมงคล",
-  "จัง",
-  "จิดานันท์ เหลืองเพียรสมุท",
-  "จิรัฏฐ์ ประเสริฐทรัพย",
-  "จิราภรณ์ วิหวา",
-];
+import "./modules/menuFilter.js";
 
 const authorInput = document.querySelector("#author-name-input");
 const authorResult = document.querySelector(".filter-options-content");
-
-const addAuthorsResult = (data) => {
-  authorResult.innerHTML = ``;
-  if (data.length > 0) {
-    data.forEach((value) => {
-      authorResult.innerHTML += `<a href="">${value}</a>`;
-    });
-  } else {
-    authorResult.innerHTML = `<p class="absolute top-2 z-10 w-full !text-center text-black opacity-50">ไม่พบข้อมูล</p>`;
-  }
-};
+const authorsList = authorResult?.children;
 
 authorInput?.addEventListener("input", (e) => {
-  const result = authors.filter((author) => author.toLowerCase().includes(e.target.value.toLowerCase()));
-  addAuthorsResult(result);
+  [...authorsList]?.forEach((author) => {
+    if (author.dataset.value.toLowerCase().includes(e.target.value.toLowerCase())) {
+      author.classList.remove("hidden");
+    } else {
+      author.classList.add("hidden");
+    }
+  });
 });
-
-addAuthorsResult(authors);
 
 gsap.fromTo(".author-card", { autoAlpha: 0, scale: 0.75 }, { autoAlpha: 1, scale: 1, stagger: 0.05, duration: 1, ease: "expo" });
 
