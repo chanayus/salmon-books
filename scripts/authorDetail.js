a2e246713801b4e09b660fb9b5fafec55ad65bc9
chanayus <chanayus1526@hotmail.com>
2023-10-05 16:27:42 +0700

    Update authorDetail.js

--- scripts/authorDetail.js
+++ scripts/authorDetail.js
@@ -16,7 +16,7 @@ gsap
   .fromTo("#author-image", { autoAlpha: 0, scale: 0.9 }, { autoAlpha: 1, scale: 1, ease: "expo", duration: 1 })
   .fromTo("#author-image img", { scale: 1.35 }, { scale: 1, ease: "expo", duration: 1 }, "<")
   .fromTo("#author-name", { autoAlpha: 0, y: 15 }, { autoAlpha: 1, y: 0, ease: "expo" }, "-=0.7")
-  .fromTo(".block-editor > *", { autoAlpha: 0, y: 17 }, { autoAlpha: 1, y: 0, stagger: 0.08, ease: "expo" }, "-=0.45")
+  .fromTo(".block-editor *:not(p span)", { autoAlpha: 0, y: 17 }, { autoAlpha: 1, y: 0, stagger: 0.07, ease: "expo" }, "-=0.45")
   .fromTo("#author-social a", { autoAlpha: 0, y: 10 }, { autoAlpha: 1, y: 0, stagger: 0.035, ease: "expo" }, "-=0.35")
   .fromTo(".author-books", { autoAlpha: 0 }, { autoAlpha: 1 }, "-=0.25");
 
