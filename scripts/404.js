fd6036f31a0452f335440ebebf3aadaaa12112c3
chanayus <chanayus1526@hotmail.com>
2023-10-12 20:14:59 +0700

    add initial hide

--- scripts/404.js
+++ scripts/404.js
@@ -1,4 +1,5 @@
 gsap
   .timeline()
+  .set("#main-content", { autoAlpha: 1 })
   .fromTo("#main-content h2 span", { yPercent: 105 }, { yPercent: 0, stagger: 0.125, duration: 1.5, ease: "expo.inOut" }, "-=0.25")
   .fromTo(["#main-content p", "#main-content a"], { autoAlpha: 0 }, { autoAlpha: 1, duration: 1.5, ease: "expo" }, "-=0.35");
