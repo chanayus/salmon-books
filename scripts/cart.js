1daa850238128603ac58553acb18659caca7c8f1
chanayus <chanayus1526@hotmail.com>
2023-10-27 17:31:33 +0700

    update

--- scripts/cart.js
+++ scripts/cart.js
@@ -1,1 +1,14 @@
 import "./modules/product/productIncrement.js";
+
+const increaseBtn = document.querySelectorAll(".product-increment .increase");
+const decreaseBtn = document.querySelectorAll(".product-increment .decrease");
+
+const updateCartBtn = document.querySelector("#update-cart-button");
+
+[...increaseBtn, ...decreaseBtn]?.forEach((btn) => {
+  btn?.addEventListener("click", () => {
+    if (updateCartBtn) {
+      updateCartBtn.disabled = false;
+    }
+  });
+});
