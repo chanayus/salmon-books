a4cca5a3ff16db7bf77f1e12c8157fd5e63634ae
chanayus <chanayus1526@hotmail.com>
2023-11-03 11:23:52 +0700

    update

--- scripts/checkout.js
+++ scripts/checkout.js
@@ -1,8 +1,15 @@
 import "./modules/inputPassword.js";
-
 const couponForm = document.querySelector("#coupon-form");
+const loginForm = document.querySelector("#login-form");
+
+const loginToggle = document.querySelector("#login-form-toggle");
 const couponToggle = document.querySelector("#coupon-toggle");
 
+loginToggle?.addEventListener("click", () => {
+  loginForm?.classList.toggle("grid-rows-[0fr]");
+  loginForm?.classList.toggle("grid-rows-[1fr]");
+  loginForm?.classList.toggle("py-3");
+});
 couponToggle?.addEventListener("click", () => {
   couponForm?.classList.toggle("grid-rows-[0fr]");
   couponForm?.classList.toggle("grid-rows-[1fr]");
