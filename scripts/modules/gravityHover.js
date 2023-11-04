66f1a1a76dde952c21b812bb2ce642b9554603ef
chanayus <chanayus1526@hotmail.com>
2023-11-04 14:51:59 +0700

    update gravity animation

--- scripts/modules/gravityHover.js
+++ scripts/modules/gravityHover.js
@@ -1,16 +1,21 @@
+const randomNumber = () => {
+  const num = Math.ceil(Math.random() * 24 - 12);
+  return num < 6 && num > -6 ? 6 : num;
+};
+
 export const gravityHover = (selector) => {
   const cards = document.querySelectorAll(selector);
 
   cards?.forEach((card) => {
     card.addEventListener("mouseenter", (e) => {
       if (!card.classList.contains("gravity-hover")) {
-        const x = e.layerX < 50 ? 7 : -7;
-        const y = e.layerY < 50 ? 7 : -7;
+        const x = randomNumber();
+        const y = randomNumber();
 
         // Set unique animation values for each card
         card.style.setProperty("--gravity-x", `${x}px`);
         card.style.setProperty("--gravity-y", `${y}px`);
-
+        console.log(x, y);
         card.classList.add("gravity-hover");
       }
     });
